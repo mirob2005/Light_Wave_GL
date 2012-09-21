0404cdd73484c2b9d08032d0dc2cfdccae9ad1e1
Michael Robertson <mirob2005@gmail.com>
2012-09-20 20:12:43 -0700

    Doing direct lighting in the fragment shader, how to do indirect lighting?

--- Light_Wave_GL/fragShader.fs
+++ Light_Wave_GL/fragShader.fs
@@ -2,11 +2,10 @@
 
 uniform sampler2D ShadowMap;
 
-varying vec4 ShadowCoord;
-varying vec4 direct_color;
-varying vec4 indirect_color;
+varying vec3 normalized_normal,normalized_vertex_to_light_vector,lightDir,normalized_light_to_vertex_vector;
 
-//varying vec4 debugOutput;
+varying vec4 ShadowCoord;
+varying vec4 indirect_color;
 
 void main( ){
 	
@@ -22,6 +21,29 @@ void main( ){
  	if (ShadowCoord.w >= 0.0)
  		shadow = distanceFromLight < shadowCoordinateWdivide.z ? 0.0 : 1.0 ;
 
+		
+	
+/*
+ *	Primary Lighting Calculations
+ */
+    vec4 direct_color = vec4(0.0,0.0,0.0,1.0);
+	 
+	vec3 n = normalize(normalized_normal);
+	vec3 VtL = normalize(normalized_vertex_to_light_vector);
+	
+	vec3 ldir = normalize(lightDir);
+	vec3 LtV = normalize(normalized_light_to_vertex_vector);
+	
+  	// Reflection term of object
+	float DiffuseTermObj = clamp(dot(n, VtL), 0.0, 1.0);
+	
+	//Reflection term of directional light
+	float DiffuseTermLight = clamp(dot(ldir, LtV),0.0,1.0);
+
+	// Calculating The Color from the primary light
+	direct_color += gl_Color*DiffuseTermLight*DiffuseTermObj;	
+/////////////////////////////////////////////////////////////////
+	
 	//gl_FragColor =	 direct_color;
 	//gl_FragColor =	 (direct_color*shadow);
 	//gl_FragColor = indirect_color;
