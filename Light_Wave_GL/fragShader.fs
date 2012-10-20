dbb4885608becc7ca7afcf0e41765391e2247227
Michael Robertson <mirob2005@gmail.com>
2012-10-19 20:54:59 -0700

    Retooled direct lighting in shaders, added specular to indirect, got rid of commented/no longer used code

--- Light_Wave_GL/fragShader.fs
+++ Light_Wave_GL/fragShader.fs
@@ -4,8 +4,7 @@
 
 uniform sampler2DArrayShadow ShadowMap;
 
-varying vec3 normalized_normal,normalized_vertex_to_light_vector,lightDir,normalized_light_to_vertex_vector;
-varying vec3 normalized_viewing_position_vector;
+varying vec3 lightDir, lightDirRef, camDir, normal;
 
 varying vec4 ShadowCoord;
 varying vec4 INDShadowCoord[20];
@@ -13,7 +12,7 @@ varying vec4 indirect_color;
 
 void main( ){
 	
-	//Shadow Mapping
+	//DIRECT Shadow Mapping
 	float shadow = 0.0;
 	shadow += shadow2DArray(ShadowMap, vec4(ShadowCoord.xy / ShadowCoord.w, 0, ShadowCoord.z / ShadowCoord.w)).r;
 
@@ -24,50 +23,26 @@ void main( ){
 		INDshadow += shadow2DArray(ShadowMap, vec4(INDShadowCoord[i].xy / INDShadowCoord[i].w, i+1, INDShadowCoord[i].z / INDShadowCoord[i].w)).r;
 	}
 	
-	INDshadow /= 20.0;	
+	INDshadow /= 10.0;	
 	
 /*
  *	Primary Lighting Calculations
  */
  
     // DIFFUSE
-    vec4 direct_color = vec4(0.0,0.0,0.0,1.0);
-	 
-	vec3 n = normalize(normalized_normal);
-	vec3 VtL = normalize(normalized_vertex_to_light_vector);
-	
-	vec3 ldir = normalize(lightDir);
-	vec3 LtV = normalize(normalized_light_to_vertex_vector);
-	
-  	// Reflection term of object
-	float DiffuseTermObj = clamp(dot(n, VtL), 0.0, 1.0);
-	
-	//Reflection term of directional light
-	float DiffuseTermLight = clamp(dot(ldir, LtV),0.0,1.0);
-	
-	// Calculating The Color from the primary light
-	direct_color += gl_Color*DiffuseTermLight*DiffuseTermObj;
-	
+	float DiffuseTerm = max(dot(normalize(lightDir), normal), 0.0);
 	
 	//SPECULAR
-	vec3 viewVec = normalize(normalized_viewing_position_vector);
-	float SpecularTerm = 0.0;
-	float normal_dot_light = dot(n,VtL);
-	if(normal_dot_light > 0.0)
-	{
-	  vec3 Reflection = vec3(2.0*vec3(n)*normal_dot_light)-VtL;
-	  SpecularTerm = dot(Reflection, viewVec);
-	}	
-	if(SpecularTerm > 0.0)
-	{
-	  direct_color += gl_Color*0.5*pow(SpecularTerm,10);	
-	}
-/////////////////////////////////////////////////////////////////
+	float SpecularTerm = pow(max(dot(normalize(lightDirRef), normalize(camDir)), 0.0), 32.0);
 	
-	//gl_FragColor =	 direct_color;
+	vec4 direct_color = vec4(vec3(gl_Color) * (DiffuseTerm * 0.75 + SpecularTerm), 1.0);
+	
+ 
+/////////////////////////////////////////////////////////////////
+	//gl_FragColor = direct_color;
 	//gl_FragColor =	 (direct_color*shadow);
 	//gl_FragColor = indirect_color;
-	//gl_FragColor = indirect_color*INDshadow;
+	//gl_FragColor = (indirect_color*INDshadow);
 	//gl_FragColor =	 (direct_color*shadow) + (indirect_color);
     gl_FragColor =	 (direct_color*shadow) + (indirect_color*INDshadow);
     //gl_FragColor =	 (direct_color*0.5*shadow) + (direct_color*0.5) + (0.2*indirect_color) + (0.8*indirect_color*INDshadow);
